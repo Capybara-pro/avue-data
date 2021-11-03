2fc377a15465d04adb72ac3eacb07a661ff060ad
chenhua <39904158+ch106@users.noreply.github.com>
2021-11-03 17:56:03 +0800

    avueData init

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# avue-data
-使用avue-data、dataV、avue搭建的大屏数据展示系统
+# avue-data、dataV、avue搭建的大屏数据展示系统
+```
+1、无组件间交互
+   -静态页面使用avue-data快速构建
+   -导出后，拆分整合成.vue页面，参考src/views/main.vue
+2、需要交互，比如点击切换数据、搜索等
+   -图表使用dataV的echarts
+   -页面元素使用avue或elementUI
+```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
---
+++ vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: '@import "@/styles/echart.scss";'
+      }
+    }
+  },
+  transpileDependencies: [
+    'monaco-editor',
+    '@jiaminghi/data-view'
+  ],
+  chainWebpack: (config) => {
+    //忽略的打包文件
+    config.externals({
+      'vue': 'Vue',
+      // 'vue-router': 'VueRouter',
+      'vuex': 'Vuex',
+      'axios': 'axios',
+      'element-ui': 'ELEMENT',
+    })
+  }
+}
